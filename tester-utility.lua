49a2252cb1054cf367fb8a73e529b3ad93bd27fb
炕 <166161143+deleter4@users.noreply.github.com>
2026-08-24 20:24:35 +0100

    upload tester-utility.lua

---
+++ tester-utility.lua
@@ -0,0 +1,2 @@
+-- this is pointing to another repo and may expire, backup the source code just incase.
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GmilerlolYT/ExecutorTester/refs/heads/main/Hi"))()
