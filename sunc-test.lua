09081b1d2799ae8f1f09cbd1cabdad6543276ad3
炕 <166161143+deleter4@users.noreply.github.com>
2026-08-24 20:23:16 +0100

    Create sunc-test.lua

---
+++ sunc-test.lua
@@ -0,0 +1,7 @@
+getgenv().sUNCDebug = {
+    ["printcheckpoints"] = false, -- boolean
+    ["delaybetweentests"] = 0, -- seconds
+    ["printtesttimetaken"] = true, -- boolean
+}
+
+loadstring(game:HttpGet("https://script.sunc.su/"))()
